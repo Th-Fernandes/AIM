83aeea97e32c0fa9de103fe5a29688904da1bcf1
unknown <Thiago Fernandes>
2021-11-18 17:03:16 -0300

    time over modal

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const target = {
     // ao clicar, contabiliza pontos
     createTarget.addEventListener('click', () => {
       score++
-
+      //imprime a pontuação na tela
       const printScore = document.querySelector('#score')
       printScore.textContent = `${score} shoots!!`
       printScore.classList.add('active')
@@ -33,27 +33,45 @@ const target = {
 
     const positions = () =>  this.position() 
 
-    setInterval(() => {
+    const printPositions = setInterval(() => {
       target.style.top = `${positions().top}%`
       target.style.left = `${positions().left}%`
-    }, 4000)
-
+    }, 900)
+    //para de gerar novas posições pro alvo quando acaba o tempo  
+    const verifyTime = setInterval(() => {
+      if(gameConfig.time <= 0) {
+        clearInterval(printPositions)
+        clearInterval(verifyTime)
+      }
+    }, 1000);
   }
 }
 
 const gameConfig = {
+  time: 60,
   //cria um timer de 120 segundos e imprime na tela
   timer () {
-    let time = 120
     const timer = setInterval(() => {
-      if(time <= 0) clearInterval(timer)
+      if(this.time <= 0) {
+        //para o timer
+        clearInterval(timer)
+        // exibe a mensagem de tempo esgotado
+        const timeOver = document.querySelector('.time-out')
+        timeOver.classList.toggle('active')
+
+        const finalScore = document.querySelector('#finalScore')
+        const score = document.querySelector('#score')
+        finalScore.textContent = `você acertou ${score.textContent[0] + score.textContent[1]} disparos com precisão`
+        console.log(score.textContent)
+      }
+      
       
       const timeOnScreen = document.querySelector('.timer')
 
-      if(time <= 10) timeOnScreen.style.color = "#f48d79"
+      if(this.time <= 10) timeOnScreen.style.color = "#f48d79"
 
-      timeOnScreen.textContent = time
-      time--
+      timeOnScreen.textContent = this.time
+      this.time--
     },1000)
   }
 }
